738b40cfeaba3ad95841e69a6642d4edcda5f2b7
3agroga <190509610@firat.edu.tr>
2023-12-03 14:11:28 +0300

    Adding the 2nd task

---
+++ Python/Session01/Basic_Tasks/Basic_Tasks.py
@@ -0,0 +1,17 @@
+import re
+# count the frequency of number 4
+
+# list_numbers=[1,2,4,4,5,6,7,4,9,4,4,4,4]
+
+# count = list_numbers.count(4)
+# print(count)
+
+# -------------------------
+
+# find if the letter is a Vowel
+
+# theletter=input("Today's letter: ")
+# if re.match(r'[aeoui]',theletter,re.IGNORECASE):
+#     print(f"{theletter} is a vowel")
+# else:
+#     print(f"{theletter} is not a vowel")
