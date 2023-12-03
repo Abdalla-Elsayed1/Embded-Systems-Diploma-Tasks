02aebc813537e39e4a1a4b04a9e762ed983a08f5
3agroga <190509610@firat.edu.tr>
2023-12-03 14:26:55 +0300

    adding the last task

--- Python/Session01/Basic_Tasks/Basic_Tasks.py
+++ Python/Session01/Basic_Tasks/Basic_Tasks.py
@@ -1,4 +1,3 @@
-import re
 # count the frequency of number 4
 
 # list_numbers=[1,2,4,4,5,6,7,4,9,4,4,4,4]
@@ -9,9 +8,21 @@ import re
 # -------------------------
 
 # find if the letter is a Vowel
+#import re
 
 # theletter=input("Today's letter: ")
 # if re.match(r'[aeoui]',theletter,re.IGNORECASE):
 #     print(f"{theletter} is a vowel")
 # else:
 #     print(f"{theletter} is not a vowel")
+
+#---------------------------
+
+# using the os module to accesses environment variables
+
+# import os
+# import os.path
+
+# print("Enviorenment Variables:")
+# for key,value in os.environ.items():
+#     print([f"{key},{value}"])
