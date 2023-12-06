70d454fde663139fd365c59eac828c86522686a1
3agroga <190509610@firat.edu.tr>
2023-12-06 19:25:42 +0300

    adding 2nd Task (Favourite Links)

---
+++ Python/Session03/Task02/fl.py
@@ -0,0 +1,18 @@
+import webbrowser
+favourite_links= {"chatgpt":"https://chat.openai.com/",
+                  "pythonorg":"https://www.python.org/",
+                  "w3school":"https://www.w3schools.com/",
+                  "github":"https://github.com/"
+                }
+
+def open_link(url_key):
+    url_key = favourite_links[url_key]
+    firefox_path = r"C:\Program Files\Mozilla Firefox\\firefox.exe"
+    webbrowser.register("firefox", None, webbrowser.BackgroundBrowser(firefox_path))
+    try:
+        webbrowser.get('firefox').open(url_key)
+    except:
+        print(f"Error: URL key '{url_key}' not found in favourite_links.")
+        return
+
+
---
+++ Python/Session03/Task02/main.py
@@ -0,0 +1,8 @@
+from fl import *
+
+website = input("What would you like to open ? ").lower()
+open_link(website)
+
+
+
+
